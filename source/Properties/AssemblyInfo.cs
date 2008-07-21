6e8c0e60cb6ba3f1079efed0002bcc14cb1235e2
Warren Kaufman <discomurray@gmail.com>
2008-07-21 12:29:46 -0600

    Renamed to ZeeGraph
    Changed SNK
    
    Moved Project Files to root

--- source/Properties/AssemblyInfo.cs
+++ source/Properties/AssemblyInfo.cs
@@ -1,39 +1,39 @@
 ﻿using System.Reflection;
+using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security;
-using System.Resources;
+using System.Security.Permissions;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle( "ZedGraph" )]
-[assembly: AssemblyDescription( "ZedGraph Library" )]
-[assembly: AssemblyConfiguration( "" )]
-[assembly: AssemblyCompany( "" )]
-[assembly: AssemblyProduct( "ZedGraph Library" )]
-[assembly: AssemblyCopyright( "Copyright © 2003-2007 John Champion" )]
-[assembly: AssemblyTrademark( "" )]
-[assembly: AssemblyCulture( "" )]
+[assembly: AssemblyTitle("ZeeGraph")]
+[assembly: AssemblyDescription("ZeeGraph Library")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ZeeGraph Library")]
+[assembly: AssemblyCopyright("Copyright © ZeeGraph Project 2008, John Champion 2003-2007")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible( false )]
+[assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid( "a552bf32-72a3-4d27-968c-72e7a90243f2" )]
+[assembly: Guid("256a0993-30d7-49bb-b904-1b91d62942fa")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Revision and Build Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
-[assembly: AssemblyVersion( "5.1.4.*" )]
-//[assembly: AssemblyFileVersion( "4.9.7.0" )]
-//[assembly: AllowPartiallyTrustedCallers ]
-[assembly: NeutralResourcesLanguageAttribute( "" )]
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguage("")]
