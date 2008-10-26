381123883b07ec4d4b473139efcc6d7e03a873e6
Warren Kaufman <discomurray@gmail.com>
2008-10-25 21:01:34 -0600

    Add a unit test project to the Solution.
    If we are going to expect that this is a high quality library we should
    have a some unit tests at the very least.

--- tests/Properties/AssemblyInfo.cs
+++ tests/Properties/AssemblyInfo.cs
@@ -1,22 +1,18 @@
 ﻿using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-using System.Security.Permissions;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ZeeGraph")]
-[assembly: AssemblyDescription("ZeeGraph Library")]
+[assembly: AssemblyTitle("ZeeGraph.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ZeeGraph Library")]
-[assembly: AssemblyCopyright("Copyright © ZeeGraph Project 2008, John Champion 2003-2007")]
+[assembly: AssemblyProduct("ZeeGraph Tests")]
+[assembly: AssemblyCopyright("Copyright © ZeeGraph Project 2008")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-[assembly: InternalsVisibleTo("ZeeGraph.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100d9f819bbbf3e501b6e367d0b149e84af9985da76e180b35cae6e5da6fabccc2edf0db8d700043c9b5496d04eff50ce8927dc26b4425b534e28bae051c5b3c9ffda2417c759ee79e7aff6aa41f8ade0a9bdf4d620037cd59674773086c77b654efd38f7c96e498010ca6e98e9375d467ee34924388b343ea7c27a41c5cd5254b8")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
@@ -24,7 +20,7 @@ using System.Security.Permissions;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("256a0993-30d7-49bb-b904-1b91d62942fa")]
+[assembly: Guid("f80f7b51-ea96-4ba3-bd2d-cf7b4066e374")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -38,4 +34,3 @@ using System.Security.Permissions;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: NeutralResourcesLanguage("")]
